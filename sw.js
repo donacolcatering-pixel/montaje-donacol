b70a92e9f8c53524d846b9dbe4d4658114a002bd
App Pedidos Dona Col <app@donacol.es>
2026-06-15 15:22:30 +0200

    Checklist: boton para eliminar fotos subidas (quita de la galeria y de Firestore)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker: cachea la app para que funcione sin internet
-const CACHE = 'montaje-v4';
+const CACHE = 'montaje-v5';
 const ASSETS = ['./', './index.html', './necesidades.html', './montaje2d.html',
                 './manifest.json', './icon.svg', './logo.png'];
 
