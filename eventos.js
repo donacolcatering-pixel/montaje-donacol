0d6dc4613058ad673c4d451775ac78b3f0d16a17
App Pedidos Dona Col <app@donacol.es>
2026-07-15 12:10:42 +0200

    Eventos de montaje 2026-07-15 12:10

--- eventos.js
+++ eventos.js
@@ -1,3 +1,3 @@
 // Generado automáticamente por la app de pedidos Doña Col — NO editar a mano.
 // Datos CIFRADOS: la clave va en el enlace de la app (#k=...).
-window.EVENTOS_MONTAJE_CIFRADO = {"n": "5losK8NNpeWQ4yLN", "d": "F660aelJIk4trn7IBYqOZXe7gYbH31+CwlwXPKJN0+sywoT5nn/x4PMDj498cqoX7/9284i+yZkHf5A6xWkfhbqFUWyZXQG1lUrbLI9P5ZJ26hb9rYbWh53pmUjTnmbwaIpYzxn3gR8Gmy227k8J3sewpTxjEY/vD4sFzoa/mqhVFTfDISuwFZhqT3ggBxYpwGR0Sr10DwZGeh74ZfF+egz7ZDiOQr+Au5WRKiQ81aTfqmX06L8c1t7ihBGfU0WrsLslSSC9e/nH3V8KhNgUxkA+6KA/S49bxoHAIekkfQ=="};
+window.EVENTOS_MONTAJE_CIFRADO = {"n": "C93Ks0xuDwDB/HO9", "d": "Sh52Nvh79lPEHb1aVcK7mEFUpDdfjOVI0rorMnyCGa6KLxCsQCAOg7ErZJkRPLWQLl28TytFh7wv844352dA6O7/IVw5MZbPAn59rI9ExVhqd/WMCdA1WWe9Lr6kSNGa/MoFweO9qh1fr+Vy4bhzDhvlVMNqHqKtzBrAdw9nqqzyAEVCgE5QYdIbLfkFMWuakZFkGhpTNVH/cdMpyXQKebPiBriRKBaW8CS4vDHaUIE50/f3yXEVF+20AVELOIKHYqPHwSAkEoj5MPQTibzU9NWJjFy21dEhGzaT6KF3/g=="};
